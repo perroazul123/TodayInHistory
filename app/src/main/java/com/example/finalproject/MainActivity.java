da112c2fd52f3bb8d1e1334338373cc7cc9d276d
MP0T0 <zihaoli3@illinois.edu>
2019-04-23 22:40:26 -0500

    A

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -8,24 +8,70 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set activity_main on current screen
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        DatePicker simpleDatePicker = findViewById(R.id.simpleDatePicker); // initiate a date picker
+        simpleDatePicker.setCalendarViewShown(false); // set false value for the calendar shown function
+
+        Button event = findViewById(R.id.majorEvent);
+        Button birth = findViewById(R.id.births);
+        Button death = findViewById(R.id.deaths);
+        Button search = findViewById(R.id.search);
+
+
+        int day = simpleDatePicker.getDayOfMonth();
+        int month = simpleDatePicker.getMonth();
+
+        search.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v)
+            {
+                setContentView(R.layout.content_main);
             }
         });
+
+        event.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setContentView(R.layout.content_main);
+            }
+        });
+
+        birth.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setContentView(R.layout.content_main);
+            }
+        });
+
+        death.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setContentView(R.layout.content_main);
+            }
+        });
+
+
+
+
+
+
     }
 
     @Override
